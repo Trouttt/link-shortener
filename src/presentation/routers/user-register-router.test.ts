d548be4a31521d27320a97de5dee29d72c70aa10
Leonardo da Silva Siqueira <desu987@hotmail.com>
2021-12-11 18:46:22 -0300

    Chore(test): ensure UserRegisterRouter returns 400 if no email is provided

--- src/presentation/routers/user-register-router.test.ts
+++ src/presentation/routers/user-register-router.test.ts
@@ -1,1 +1,28 @@
-describe('User Register Router', () => {});
+import AccountModel from '../../domain/models/account';
+
+class UserRegisterRouter {
+  route = (httpRequest: { body: AccountModel }) => {
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+      };
+    }
+    return {
+      statusCode: 200,
+    };
+  };
+}
+
+describe('User Register Router', () => {
+  test('Should return 400 if no email is provided', () => {
+    const sut = new UserRegisterRouter();
+    const httpRequest = {
+      body: {
+        password: 'aspokdsp',
+        name: 'teste',
+      },
+    };
+    const httpResponse = sut.route(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
+});
