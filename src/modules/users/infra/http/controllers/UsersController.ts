a93b5c86c1d3a6c5e6a1a8ce3c11aefcc3182e37
Leonardo da Silva Siqueira <desu987@hotmail.com>
2021-12-16 12:58:18 -0300

    Chore(controller): added the CreateUserService to execute the user register

--- src/modules/users/infra/http/controllers/UsersController.ts
+++ src/modules/users/infra/http/controllers/UsersController.ts
@@ -1,11 +1,18 @@
 import { Request, Response } from 'express';
+import CreateUserService from '../../../services/CreateUserService';
 
 export default class UsersController {
   public async create(request: Request, response: Response): Promise<Response> {
     try {
       const { name, email, password } = request.body;
 
-      return response.json({});
+      const createUserService = new CreateUserService();
+      const user = await createUserService.execute({
+        name,
+        email,
+        password,
+      });
+      return response.json(user);
     } catch (err) {
       response.statusCode = err.statusCode;
       return response.json(err);
