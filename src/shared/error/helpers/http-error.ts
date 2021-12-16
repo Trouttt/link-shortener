90c12b81830a2de09572f283621b8197943a9349
Leonardo da Silva Siqueira <desu987@hotmail.com>
2021-12-16 12:36:47 -0300

    Added(error): added body in response ok

--- src/shared/error/helpers/http-error.ts
+++ src/shared/error/helpers/http-error.ts
@@ -1,20 +1,25 @@
 import MissingParamError from '../errors/missing-param-error';
 
+interface IResponse {
+  statusCode: number;
+  body: MissingParamError;
+}
 export default class HttpResponse {
-  static badRequest(paramName: string): any {
+  static badRequest(paramName: string): IResponse {
     return {
       statusCode: 400,
       body: new MissingParamError(paramName),
     };
   }
 
-  static ok() {
+  static ok(): IResponse {
     return {
       statusCode: 200,
+      body: new MissingParamError(''),
     };
   }
 
-  static serverError(paramName: string) {
+  static serverError(paramName: string): IResponse {
     return {
       statusCode: 500,
       body: new MissingParamError(paramName),
