5617520a3c8d3ca3029e05c016f0866670dcd1f7
Leonardo da Silva Siqueira <desu987@hotmail.com>
2021-12-16 14:19:04 -0300

    Feat(test): ensure LoginUserRouter returns error if email is invalid

--- src/modules/users/services/LoginUserService.test.ts
+++ src/modules/users/services/LoginUserService.test.ts
@@ -18,9 +18,11 @@ const fakeAccounts: AccountModel[] = [
 class LoginUserRouter {
   route = (httpRequest: { body: AccountModel }) => {
     const { email, password } = httpRequest.body;
-
     if (!email) {
       return HttpResponse.badRequest('Campo email está vazio!!!');
+    }
+    if (email.indexOf('@') === -1) {
+      return HttpResponse.badRequest('Email está inválido!!!');
     }
 
     return {
@@ -42,6 +44,21 @@ describe('User authenticate', () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(
       new MissingParamsError('Campo email está vazio!!!'),
+    );
+  });
+
+  test('Should return 400 and message if email is invalid', () => {
+    const sut = new LoginUserRouter();
+    const httpRequest = {
+      body: {
+        email: 'any_email.com',
+        password: 'any_password2',
+      },
+    };
+    const httpResponse = sut.route(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(
+      new MissingParamsError('Email está inválido!!!'),
     );
   });
 });
