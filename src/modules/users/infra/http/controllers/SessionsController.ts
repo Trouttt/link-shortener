d181e5f9bbf214f0861743cdb40db1023de148eb
Leonardo da Silva Siqueira <desu987@hotmail.com>
2021-12-18 12:18:30 -0300

    Feat(controller): added the authenticate user service and http response

---
+++ src/modules/users/infra/http/controllers/SessionsController.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express';
+import Http from '../../../../../shared/error/Http';
+import LoginUserService from '../../../services/AuthenticateUserService';
+
+export default class SessionsController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    try {
+      const { email, password } = request.body;
+
+      const http = new Http();
+      const authenticateUser = new LoginUserService();
+
+      const { user, token } = await authenticateUser.execute({
+        email,
+        password,
+      });
+
+      response.statusCode = http.response(user);
+      user.password = '';
+
+      return response.json({ user, token });
+    } catch (err) {
+      response.statusCode = err.statusCode;
+      return response.json(err);
+    }
+  }
+}
