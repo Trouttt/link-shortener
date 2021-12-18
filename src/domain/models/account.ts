9e5524a9ff520fd8691d5df5ce27bb9a150c526e
Leonardo da Silva Siqueira <desu987@hotmail.com>
2021-12-18 13:21:32 -0300

    Chore(config): added config base

--- src/domain/models/account.ts
+++
@@ -1,5 +0,0 @@
-export default interface AccountModel {
-  email?: string;
-  name?: string;
-  password?: string;
-}
--- src/server.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-console */
-import express from 'express';
-
-const app = express();
-
-app.get('/', (request, response) => response.json({ message: 'heloworld' }));
-app.listen(3333, () => console.log('hey'));
