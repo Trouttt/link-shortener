ad0b5994fc269b0ac75cb8faf7f0005956d9f50e
Leonardo da Silva Siqueira <desu987@hotmail.com>
2021-12-19 18:14:16 -0300

    Added(service): added service to create url

--- src/modules/urls/services/CreateShortUrl.test.ts
+++ src/modules/urls/services/CreateShortUrl.test.ts
@@ -1,37 +1,27 @@
 import { createConnections, getConnection } from 'typeorm';
-import AppError from '../../../shared/error/AppError';
-
-interface UrlModel {
-  url: string;
-  user_id: string;
-  short_url?: string;
-  visited: number;
-}
-
-class CreateUrlService {
-  execute = (httpRequest: { body: UrlModel }) => {
-    const { url } = httpRequest.body;
-    if (url.length === 0 || !url) {
-      return new AppError('Campo URL está vazio!!!', 400);
-    }
-    return null;
-  };
-}
+import CreateUrlService from './CreateShortUrlService';
 
 const makeSut = () => {
   return new CreateUrlService();
 };
 
+beforeAll(async () => {
+  await createConnections();
+});
+
+afterAll(async () => {
+  const defaultConnection = getConnection('default');
+  await defaultConnection.close();
+});
+
 describe('URL register service', () => {
   test('Should return 400 and message if no url is provided', async () => {
     const sut = makeSut();
     const httpRequest = {
-      body: {
-        url: '',
-        user_id: '',
-        short_url: '',
-        visited: 1,
-      },
+      url: '',
+      user_id: '',
+      shortUrl: '',
+      visited: 1,
     };
     const httpResponse = await sut.execute(httpRequest);
     expect(httpResponse).toEqual({
---
+++ src/modules/urls/services/CreateShortUrlService.ts
@@ -0,0 +1,47 @@
+import 'reflect-metadata';
+import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+import IShortUrl from '../infra/typeorm/entities/ShortUrl';
+import AppError from '../../../shared/error/AppError';
+
+interface IRequest {
+  url: string;
+  shortUrl: string;
+  user_id: string;
+  visited: number;
+}
+
+class CreateUserService {
+  public async execute({
+    url,
+    user_id,
+    visited,
+  }: IRequest): Promise<IShortUrl> {
+    try {
+      const urlsRepository = await getRepository(IShortUrl);
+
+      if (url.length === 0 || !url) {
+        throw new AppError('Campo URL está vazio!!!', 400);
+      }
+
+      const idRandom = Math.random()
+        .toString(36)
+        .replace(/[^a-z]+/g, '')
+        .substr(0, 5);
+
+      const link = await urlsRepository.create({
+        url,
+        shortUrl: `localhost:8080/${idRandom}`,
+        user_id,
+        visited,
+      });
+
+      await urlsRepository.save(link);
+
+      return link;
+    } catch (err) {
+      return err;
+    }
+  }
+}
+export default CreateUserService;
