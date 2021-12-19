5422add3f2fe7332dd2815406b235596119a96d7
Leonardo da Silva Siqueira <desu987@hotmail.com>
2021-12-19 18:15:52 -0300

    Feat(controller): added url create service in url controller

--- src/modules/urls/infra/http/controllers/UrlsController.ts
+++ src/modules/urls/infra/http/controllers/UrlsController.ts
@@ -1,15 +1,24 @@
 import { Request, Response } from 'express';
 import Http from '../../../../../shared/error/Http';
+import CreateUrlService from '../../../services/CreateShortUrlService';
 
 export default class UrlsController {
   public async create(request: Request, response: Response): Promise<Response> {
     try {
-      const { url, user_id } = request.body;
+      const { url, shortUrl, user_id, visited } = request.body;
 
       const http = new Http();
+      const createUrlService = new CreateUrlService();
+      const link = await createUrlService.execute({
+        url,
+        shortUrl,
+        user_id,
+        visited,
+      });
 
+      response.statusCode = http.response(link);
 
-      return response.json({});
+      return response.json(link);
     } catch (err) {
       response.statusCode = err.statusCode;
       return response.json(err);
