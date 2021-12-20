ddac68db188f32ce205be5836151a77b284376bf
Leonardo da Silva Siqueira <desu987@hotmail.com>
2021-12-20 00:02:58 -0300

    Feat(router): added url router in main router

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import { createConnection } from 'typeorm';
 import sessionsRouter from '../../../../modules/users/infra/http/routes/sessions.router';
-
 import usersRouter from '../../../../modules/users/infra/http/routes/users.routes';
+import urlsRouter from '../../../../modules/urls/infra/http/routes/urls.router';
 
 createConnection();
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
+routes.use('/urls', urlsRouter);
 
 export default routes;
